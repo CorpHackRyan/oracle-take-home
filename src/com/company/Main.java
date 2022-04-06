e81d7ec7460e4377a774a9664ccda7d205665a64
Ryan OConnor <r2oconnor@student.bridgew.edu>
2022-04-06 08:22:07 -0400

    created function for execution, testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,21 @@ package com.company;
 
 public class Main {
 
+    public static int return_num(int input)  {
+        return input * 2;
+    }
+
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Testing");
+        int x = 2;
+
+        int y = return_num(x);
+        System.out.println(y);
+
+
+
+
+
     }
 }
